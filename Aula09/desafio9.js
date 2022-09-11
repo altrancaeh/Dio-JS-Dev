de30556a69a23d82f914ad90346a8fb1200623a7
Gukskull <gukzuk@gmail.com>
2022-09-11 10:17:11 -0300

    Aula 9 e Desafios

---
+++ Aula09/desafio9.js
@@ -0,0 +1,10 @@
+/*
+Crie um programa que dado um número, imprima sua tabuada.
+*/
+
+let numero = 5
+for(let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero*i}`)
+}
+
+// Posição inicial é i, início da tabuada. Se ele não for menor ou igual a 10, ele realia a soma de si mesmo e multipilica  na linha 7 pelo número que aloquei na variável.
